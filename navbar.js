e99e17f8e6b1040c419bc6a83a7097ae4a4581d0
Kasper_Chen <neko12377@gmail.com>
2019-11-17 20:37:36 +0800

    圖檔更新

--- navbar.js
+++ navbar.js
@@ -3,11 +3,11 @@ $(function(){
     $(window).resize(function(){
         let x = $(window).width();
         console.log(x);
-        if(x <= 768 ){
+        if(x <= 320 ){
             $(".minusMg").removeClass("container");
             $(".navbar,.navbar-dark").addClass("px-0");
         }
-        if(x > 768) {
+        if(x > 320) {
             $(".minusMg").addClass("container");
             $(".navbar,.navbar-dark").removeClass("px-0");
         }
