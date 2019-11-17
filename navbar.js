88bb2c8df102cf44356a3250cb9a8833ada595e7
Kasper_Chen <neko12377@gmail.com>
2019-11-17 22:23:04 +0800

    use breakpoint replace js

--- navbar.js
+++ navbar.js
@@ -1,17 +1,17 @@
 $(function(){
 
-    $(window).resize(function(){
-        let x = $(window).width();
-        console.log(x);
-        if(x <= 320 ){
-            $(".minusMg").removeClass("container");
-            $(".navbar,.navbar-dark").addClass("px-0");
-        }
-        if(x > 320) {
-            $(".minusMg").addClass("container");
-            $(".navbar,.navbar-dark").removeClass("px-0");
-        }
-    })
+    // $(window).resize(function(){
+    //     let x = $(window).width();
+    //     console.log(x);
+    //     if(x <= 320 ){
+    //         $(".minusMg").removeClass("container");
+    //         $(".navbar,.navbar-dark").addClass("px-0");
+    //     }
+    //     if(x > 320) {
+    //         $(".minusMg").addClass("container");
+    //         $(".navbar,.navbar-dark").removeClass("px-0");
+    //     }
+    // })
 
     $(".fa-cog")[0].onclick = function(){
         $(".fa-cog")[0].classList.add("fa-spin");
